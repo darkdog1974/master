5163723d5e002e1f45abfbd6a137b4c19b64954d
darkdog1974 <darkdog74@hotmail.com>
2021-01-04 10:18:14 +0100

    Creazione app UMS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import {AngularFireDatabase} from '@angular/fire/database';  
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'my-app',
@@ -10,13 +8,5 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  utentiCollection: AngularFirestoreCollection<any>;
-  utenti: Observable<any[]>;
- 
-
-  constructor(afs: AngularFirestore, af: AngularFireDatabase) {
-    //this.utentiCollection = this.afs.collection("UTENTI");
-    //this.utenti = afs.collection("UTENTI").valueChanges();
-    this.utenti = af.list('/UTENTI').valueChanges();
-  }
+  constructor() {}
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {AngularFireDatabase} from '@angular/fire/database';  
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -8,8 +11,14 @@ import { Component, OnInit } from '@angular/core';
 export class UsersComponent implements OnInit {
 
   caption: string = "Users List";
+  utentiCollection: AngularFirestoreCollection<any>;
+  utenti: Observable<any[]>;
 
-  constructor() { }
+  constructor(afs: AngularFirestore, af: AngularFireDatabase) {
+    //this.utentiCollection = this.afs.collection("UTENTI");
+    //this.utenti = afs.collection("UTENTI").valueChanges();
+    this.utenti = af.list('/UTENTI').valueChanges();
+  }
 
   ngOnInit() {
   }
