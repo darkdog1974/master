46f0189622e631fbff36c6c6c9ef0ec349d5679e
darkdog1974 <darkdog74@hotmail.com>
2021-01-04 12:54:19 +0100

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  /* 
+    In questo caso il selettore viene definito come un attributo del tag <tr>
+    e non come un elemento a se stante.
+  */
+  selector: 'tr[app-user]',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
